1520204f3e72241436ae5a0aef102542e366ffea
kolodny <kolodny.github@gmail.com>
2013-07-01 14:10:07 -0400

    fix issues with github squashing script

--- github-tab-resizer.user.js
+++ github-tab-resizer.user.js
@@ -2,31 +2,34 @@
 // @name           Github Tab Resizer
 // @namespace      https://github.com/kolodny
 // @description    Let's you change the size of tabs when viweing code of a repo
-// @include        https://github.com/*
+// @match          https://github.com/*
 // @copyright      Moshe Kolodny
 // @version        1.0.0
 // @license        http://opensource.org/licenses/MIT
 // ==/UserScript==
 
-var div = document.createElement('div'),
-	select = document.createElement('select'),
-	insertBefore = document.getElementById('files');
-	
-for (var i = 0; i < 8; i++) {
-	select.innerHTML += '<option>' + i + '</option>';
-}
-select.innerHTML += '<option selected>8</option>';
-
-select.onchange = function() {
-	var selectedSize = this.options[this.options.selectedIndex].value,
-		filesDiv = document.getElementById('files');
+setTimeout(function() {
+	var div = document.createElement('div'),
+		select = document.createElement('select'),
+		insertBefore = document.getElementById('files');
 		
-	filesDiv.style['-moz-tab-size'] = selectedSize;
-	filesDiv.style['-o-tab-size'] = selectedSize;
-	filesDiv.style['tab-size'] = selectedSize;
-};
+	for (var i = 0; i < 8; i++) {
+		select.innerHTML += '<option>' + i + '</option>';
+	}
+	select.innerHTML += '<option selected>8</option>';
 
-div.innerHTML = '<label>Select Tab Size: ';
-div.appendChild(select);
+	select.onchange = function() {
+		var selectedSize = this.options[this.options.selectedIndex].value,
+			filesDiv = document.getElementById('files');
+			
+		filesDiv.style['-moz-tab-size'] = selectedSize;
+		filesDiv.style['-o-tab-size'] = selectedSize;
+		filesDiv.style['tab-size'] = selectedSize;
+	};
 
-insertBefore.parentNode.insertBefore(div, insertBefore)
+	div.innerHTML = '<label>Select Tab Size: ';
+	div.appendChild(select);
+
+	insertBefore.parentNode.insertBefore(div, insertBefore)
+}, 1000);
+
